c7e9fad5ae0f48f131bd82b554dbe27cbdde065e
Andre <andreferreira@live.co.uk>
2025-01-05 15:03:44 +0000

    unshift

---
+++ LL/LL-Unshift.js
@@ -0,0 +1,31 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  unshift(value) {
+    const newNode = new Node(value);
+    if (this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+let myLinkedList = new LinkedList(10);
+myLinkedList.unshift();
